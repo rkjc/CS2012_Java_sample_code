7974b1d2498008fbc3d826056e210154880ebc9a
rkjc <richardkjcross@gmail.com>
2021-05-04 12:24:13 -0700

    quick code update

--- BinaryIO/src/binaryio/Exercise17_07.java
+++ BinaryIO/src/binaryio/Exercise17_07.java
@@ -5,17 +5,5 @@ import java.util.Date;
 
 public class Exercise17_07 {
 	
-	public static void main(String[] args) throws ClassNotFoundException, IOException {
-		// this was developed from the TestObjectInputStream.java example
-		Double subTotal = 0.0;
-		try ( // Create an input stream for file object.dat
-				ObjectInputStream input = new ObjectInputStream(new FileInputStream("Exercise17_06.dat"));) {
 
-			Loan ln01 = (Loan) (input.readObject());
-			subTotal = ln01.getLoanAmount();
-			// hmmm ... maybe a loop to get the unknown number of additional loan objects stored in the file?
-
-			System.out.println(subTotal);
-		}
-	}
 }
